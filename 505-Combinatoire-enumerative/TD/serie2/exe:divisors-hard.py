302f0e10deae9ca108cba80535526c90dcfe47bb
Mattuuuuh <mat130599@gmail.com>
2026-09-08 14:29:55 +0300

    Moved python verbatim files up two folders.

--- 505-Combinatoire-enumerative/TD/serie2/exe:divisors-hard.py
+++
@@ -1,8 +0,0 @@
-\begin{verbatim}
-num = 1000
-for i in range(1,1001):
-    if i%8 == 0 or i%12 == 0 or i%15 == 0:
-        num -= 1
-print(num)
->>> 783
-\end{verbatim}
--- 505-Combinatoire-enumerative/TD/serie2/exe:divisors.py
+++
@@ -1,8 +0,0 @@
-\begin{verbatim}
-num = 0
-for i in range(1,1001):
-    if i%7 == 0 or i%11 == 0 or i%13 == 0:
-        num += 1
-print(num)
->>> 280
-\end{verbatim}.
